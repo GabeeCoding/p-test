2bc2d855530ac19c3e2bf4d6a8459486dba8ff21
Gabes <gabnology+github@gmail.com>
2022-07-14 17:24:37 +0000

    Fix server not listening

--- app.js
+++ app.js
@@ -18,3 +18,8 @@ let userData = {
 app.get("/", (req, resp) => {
     resp.send(req.rawHeaders.toString())
 });
+
+let port = process.env.PORT||3000
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
