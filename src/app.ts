91cfc009c118987fed876f58d719df07df22c75d
Gabes <gabnology+github@gmail.com>
2022-07-27 17:29:33 +0000

    Add user data functions

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,23 @@ let userData: [{userId: number, inviteList: {from: number, jobId: string, messag
     },
 ];
 
+function initUserData(userid:number){
+    //Adds user data to the array
+    userData.push({userId: userid, inviteList: []});
+    return userData.find(user => user.userId == userid)
+}
+
+function getUserData(userid: number){
+    //Retrieves user data from array
+    //If it doesn't exist, create it
+    let data = userData.find((data) => data.userId == userid)
+    if(data){
+        return data
+    } else {
+        return initUserData(userid);
+    }
+}
+
 app.get("/echo", (req, resp) => {
     resp.send(req.body);
 });
